7ab718b4c2595e18f9fb02b14f58086062427fba
VinCeProg <cananes.vincent.cueto@gmail.com>
2024-10-25 14:54:47 +0800

    added getter and setter

--- src/trainticketvm/Payment.java
+++ src/trainticketvm/Payment.java
@@ -7,4 +7,39 @@ public class Payment {
   private double amount;
   private Date paymentDate;
   private String paymentMethod;
+
+  
+  
+  
+  public String getPaymentID() {
+    return paymentID;
+  }
+
+  public void setPaymentID(String paymentID) {
+    this.paymentID = paymentID;
+  }
+
+  public double getAmount() {
+    return amount;
+  }
+
+  public void setAmount(double amount) {
+    this.amount = amount;
+  }
+
+  public Date getPaymentDate() {
+    return paymentDate;
+  }
+
+  public void setPaymentDate(Date paymentDate) {
+    this.paymentDate = paymentDate;
+  }
+
+  public String getPaymentMethod() {
+    return paymentMethod;
+  }
+
+  public void setPaymentMethod(String paymentMethod) {
+    this.paymentMethod = paymentMethod;
+  }
 }
